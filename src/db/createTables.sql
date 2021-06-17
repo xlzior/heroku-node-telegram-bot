36fd9cc23b6eb5e6d7398934732fc5e9282fd6d8
Wen Jun <wenjun.lye@gmail.com>
2021-06-17 19:28:51 +0800

    Create postgresql tables

---
+++ src/db/createTables.sql
@@ -0,0 +1,40 @@
+CREATE TABLE users(
+  user_id INTEGER PRIMARY KEY,
+  level INTEGER,
+  xp INTEGER,
+  pinned_message_id INTEGER,
+  idat INTEGER,
+  current_reflection_id INTEGER,
+  prev_command VARCHAR
+);
+
+CREATE TABLE reflections(
+  user_id INTEGER,
+  start_id INTEGER,
+  end_id INTEGER,
+  name VARCHAR,
+  PRIMARY KEY(user_id, start_id),
+  CONSTRAINT foreign_key_user
+    FOREIGN KEY(user_id)
+      REFERENCES users(user_id)
+);
+
+CREATE TABLE achievements(
+  user_id INTEGER,
+  type VARCHAR,
+  level SMALLINT,
+  PRIMARY KEY(user_id, type),
+  CONSTRAINT foreign_key_user
+    FOREIGN KEY(user_id)
+      REFERENCES users(user_id)
+);
+
+CREATE TABLE hashtags(
+  user_id INTEGER,
+  start_id INTEGER,
+  hashtag VARCHAR,
+  PRIMARY KEY(user_id, start_id, hashtag),
+  CONSTRAINT foreign_key_user
+    FOREIGN KEY(user_id)
+      REFERENCES users(user_id)
+);
