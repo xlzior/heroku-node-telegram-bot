c1ded2286c7a4e2659f2c396361669e868c1297c
Wen Jun <wenjun.lye@gmail.com>
2021-06-20 22:15:15 +0800

    Fix hashtags
    Chronological order (most recent first)
    On conflict do nothing

--- src/db/hashtags.js
+++ src/db/hashtags.js
@@ -11,29 +11,27 @@ const getUniqueCount = async userId => {
   return parseInt(getFirst(res).count);
 };
 
-const get = async userId => {
-  const hashtags = await pool.query(
-    `SELECT hashtag, json_agg(json_build_array(hashtags.start_id, name) ORDER BY hashtags.start_id) AS messages
+const get = userId => {
+  return pool.query(
+    `SELECT
+      hashtag,
+      json_agg(json_build_array(hashtags.start_id, name) ORDER BY hashtags.start_id DESC) AS messages
     FROM hashtags
-    INNER JOIN reflections
-    ON (hashtags.start_id = reflections.start_id)
+    INNER JOIN reflections ON (hashtags.start_id = reflections.start_id)
     WHERE hashtags.user_id=${userId}
     GROUP BY hashtag;`).then(getRows);
-
-  return hashtags;
 };
 
 const add = async (userId, hashtags = []) => {
   if (hashtags.length === 0) return;
   const startId = await current.getId(userId);
   const promises = hashtags.map(hashtag => {
-    return pool.query(`INSERT INTO hashtags(user_id, start_id, hashtag) VALUES(${userId}, ${startId}, '${hashtag}');`);
+    return pool.query(
+      `INSERT INTO hashtags(user_id, start_id, hashtag)
+      VALUES(${userId}, ${startId}, '${hashtag}')
+      ON CONFLICT DO NOTHING;`);
   });
-  try {
-    await Promise.all(promises);
-  } catch (e) {
-    console.info(e);
-  }
+  return Promise.all(promises);
 };
 
 module.exports = {
