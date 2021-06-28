ba16e0614be4b119d4cd050a0f9c37acad3ce687
Wen Jun <wenjun.lye@gmail.com>
2021-06-28 19:06:14 +0800

    Update scheduled to not interrupt ongoing journalling sessions

--- src/scheduled.js
+++ src/scheduled.js
@@ -15,14 +15,16 @@ const main = async () => {
   const schedules = await db.schedules.getTime(now);
 
   await Promise.all(schedules.map(async ({ user_id: userId, questions }) => {
-    const message = [
-      "It's time for your scheduled journalling session!",
-      `Here's your first prompt: ${questions[0]}`,
-      "When finished, send /done. You may use /skip to skip this journalling session.",
-    ].join("\n\n");
-    const botMsg = await bot.sendMessage(userId, message);
-    await db.reflections.open(userId, botMsg.message_id);
-    await db.users.prevCommand.set(userId, "scheduled", { index: 1, time: now });
+    if (!db.reflections.isOpen(userId)) { // don't interupt an ongoing session
+      const message = [
+        "It's time for your scheduled journalling session!",
+        `Here's your first prompt: ${questions[0]}`,
+        "When finished, send /done. You may use /skip to skip this journalling session.",
+      ].join("\n\n");
+      const botMsg = await bot.sendMessage(userId, message);
+      await db.reflections.open(userId, botMsg.message_id);
+      await db.users.prevCommand.set(userId, "scheduled", { index: 1, time: now });
+    }
   }));
 
   server.close(process.exit);
