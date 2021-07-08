2fdee5932f33e1e1ba055ad987b74f095c9a4e63
Wen Jun <wenjun.lye@gmail.com>
2021-07-08 11:23:26 +0800

    Update /help message

--- src/bot/basic.js
+++ src/bot/basic.js
@@ -1,9 +1,10 @@
 const db = require("../db");
-const utils = require("../utils");
-const { formatLevel } = require("../levels");
 const errors = require("../db/errors");
-
+const utils = require("../utils");
 const { clean, MARKDOWN } = utils.telegram;
+const { formatLevel } = require("../levels");
+
+const helpMessage = require("./help");
 
 function handleBasic({ bot }) {
   bot.onText(/\/start/, async ({ send, chatId }, msg) => {
@@ -32,46 +33,7 @@ function handleBasic({ bot }) {
   });
 
   bot.onText(/\/help/, ({ send }) => {
-    const intro = [
-      "Welcome to LifeXP, a gamified journalling chatbot.",
-      "I'm here to help you pen down your thoughts in a safe and convenient environment.",
-    ].join("\n");
-    const journal = [
-      "*Journal*",
-      "/open - start a new journal entry",
-      "/prompt - get a randomised prompt",
-      "/echo - give yourself a prompt",
-      "/ididathing - celebrate something you're proud of",
-      "/close - end off the journal entry",
-    ].join("\n");
-    const browse = [
-      "*Browse*",
-      "/reflections - list all reflections",
-      "/hashtags - list all hashtags",
-      "/hashtag - browse reflections with a particular hashtag",
-    ].join("\n");
-    const game = [
-      "*Game Features*",
-      "/lifexp - show level and XP",
-      "/achievements - show display cabinet of achievement badges",
-      "/stats - show statistics",
-    ].join("\n");
-    const scheduled = [
-      "*Scheduled Journalling Sessions*",
-      "/manage_schedules",
-      "/add_schedule",
-      "/edit_schedule",
-      "/delete_schedule",
-    ].join("\n");
-    const misc = [
-      "/cancel - cancel your previous command",
-    ].join("\n");
-    const outro = [
-      "I hope you have a meaningful journalling session. 😊",
-    ].join("\n");
-
-    const message = [intro, journal, browse, game, scheduled, misc, outro].join("\n\n");
-    send(clean(message), MARKDOWN);
+    send(clean(helpMessage.join("\n\n")), MARKDOWN);
   });
 
   bot.onText(/\/cancel/, async ({ send, chatId }) => {
