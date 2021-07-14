efdb83da9a5f41a7295ce65738f175f7d353cf43
Wen Jun <wenjun.lye@gmail.com>
2021-07-14 16:32:58 +0800

    Convert postgresql.js to .ts

---
+++ src/db/postgresql.ts
@@ -0,0 +1,12 @@
+import pg = require("pg");
+const { Pool } = pg;
+
+export const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
+
+export const getRows = res => res.rows;
+export const getFirst = res => res.rows[0];
