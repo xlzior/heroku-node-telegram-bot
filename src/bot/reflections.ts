509528a55d9317a486ac894ee7e8a22e7ba38faf
Wen Jun <wenjun.lye@gmail.com>
2021-07-28 16:52:14 +0800

    Implement /open from a previously sent message

--- src/bot/reflections.ts
+++ src/bot/reflections.ts
@@ -1,6 +1,7 @@
 import * as db from "../db";
 import { REFLECTION_ALREADY_OPEN } from "../db/errors";
 import { getRandomPrompt, telegram, countEmojis } from "../utils";
+import { replyTo } from "../utils/telegram";
 import { HandlerArguments, OPEN_REFLECTION, CLOSE_REFLECTION } from "../types/continueConversation";
 
 export default function handleReflections({ bot, continueConversation }: HandlerArguments): void {
@@ -18,8 +19,14 @@ export default function handleReflections({ bot, continueConversation }: Handler
 
   bot.handle(/\/open/, async ({ send, chatId }, msg) => {
     try {
-      await db.reflections.open(chatId, msg.message_id);
-      send("Let's start a journalling session! If you need a prompt, you can use /prompt. If not, just start typing and I'll be here when you need me.");
+      if (msg.reply_to_message) {
+        const replyId = msg.reply_to_message.message_id;
+        await db.reflections.open(chatId, replyId);
+        send("Alright, starting a journalling session from this message onwards.\n\nIf you meant to start a fresh journalling session, please /cancel this reflection and then send /open on its own (instead of as a reply).", replyTo(replyId));
+      } else {
+        await db.reflections.open(chatId, msg.message_id);
+        send("Let's start a journalling session! If you need a prompt, you can use /prompt. If not, just start typing and I'll be here when you need me.");
+      }
       await db.users.prevCommand.set(chatId, OPEN_REFLECTION);
     } catch (error) {
       if (error === REFLECTION_ALREADY_OPEN) {
